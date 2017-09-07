8f7658ed97898911c09fef4a6c009de0cc491027
Ivan Shtarbanov <haifischy91@gmail.com>
2017-09-07 11:54:14 +0300

    Added change password form, fixed some errors, added some styles

--- src/app/forum/forum-thread-create/forum-thread-create.component.ts
+++ src/app/forum/forum-thread-create/forum-thread-create.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastsManager } from 'ng2-toastr';
 import { Router } from '@angular/router';
 import { ForumService } from './../../_core/forum-service/forum.service';
@@ -9,15 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forum-thread-create.component.css']
 })
 export class ForumThreadCreateComponent implements OnInit {
-  title;
-  content;
-  category;
+  threadCreateForm: FormGroup;
+  title: FormControl;
+  content: FormControl;
+  category: FormControl;
 
   constructor(private router: Router,
     private forumService: ForumService,
     private toastr: ToastsManager) { }
 
   ngOnInit() {
+    this.title = new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(50)
+    ]);
+    this.content = new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(200)
+    ]);
+    this.category = new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+      Validators.maxLength(15)
+    ]);
+
+    this.threadCreateForm = new FormGroup({
+      title: this.title,
+      content: this.content,
+      category: this.category,
+    });
   }
 
   createThread(threadData) {
--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,6 +1,9 @@
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from './../../_core/users-service/users.service';
 import { Component, OnInit } from '@angular/core';
+
+import { UserValidator } from '../user-validator';
 
 @Component({
   selector: 'app-profile',
@@ -9,10 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
   profile;
+
+  changePassForm: FormGroup;
+  oldPass: FormControl;
+  newPass: FormControl;
+  newPassRepeat: FormControl;
+
+  controlsCollapsed = true;
   constructor(private userService: UsersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.profile = this.route.snapshot.data['profile'];
-  }
 
+    this.oldPass = new FormControl('', UserValidator.passHashValidators);
+    this.newPass = new FormControl('', UserValidator.passHashValidators);
+    this.newPassRepeat = new FormControl('', UserValidator.passHashValidators);
+
+    this.changePassForm = new FormGroup({
+      oldPassHash: this.oldPass,
+      newPassHash: this.newPass,
+      newPassHashRepeat: this.newPassRepeat
+    });
+  }
+  
+  changePassword(data) {
+    console.log(data);
+  }
 }
