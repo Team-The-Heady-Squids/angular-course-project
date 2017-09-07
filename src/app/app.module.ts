c2ccfddb430187a6568895464c7f525aaede2545
DESKTOP-U58B01U\Masterpice2 <sillimanite90@gmail.com>
2017-09-07 15:53:52 +0300

    Added footer for the site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { MyOptions } from './toaster.options';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr';
+import { MyOptions } from './toaster.options';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { CoreModule } from './_core/core.module';
 import { HeaderModule } from './header/header.module';
+import { FooterModule } from './footer/footer.module';
 
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule,
-    HeaderModule,
     AppRoutingModule,
+    HeaderModule,
+    FooterModule,
     ToastModule.forRoot(),
     CoreModule // Contains global services
   ],
